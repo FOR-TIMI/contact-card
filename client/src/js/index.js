2b1a4642df97b664685a4757be9046f3f23388d5
FOR-TIMI <timiplayskeys@gmail.com>
2022-11-03 05:22:21 -0400

    added webPack plugin and css loader

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -9,6 +9,13 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+/** CSS */
+import '../css/index.css'
+
+/** Bootstrap*/
+import {Tooltip, Toast, Popover} from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 window.addEventListener('load', function () {
     document.getElementById('logo').src = Logo;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
@@ -10,10 +11,32 @@ module.exports = {
             test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: 'asset/resource',
           },
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ]
+              }
+            }
+          }
         ]
     },
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './index.html',
+          title: 'Webpack Plugin',
+        })
+    ]
 }
