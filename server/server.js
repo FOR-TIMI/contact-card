fa261cbbc45bf4449bdcec929585b876527ba44a
FOR-TIMI <timiplayskeys@gmail.com>
2022-11-03 02:34:57 -0400

    added server folder

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static('../client/'))
+
+require('./routes/htmlRoutes.js')(app);
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
