cdb465903c146fe7fb1d151681032f9f65e9c34d
FOR-TIMI <timiplayskeys@gmail.com>
2022-11-04 05:32:21 -0400

    restructured routes to use static distribution files

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = function(app){
     app.get('/', (req,res) => {
-        res.sendFile(path.join(__dirname, '../../client/index.html'));
+        res.sendFile(path.join(__dirname, '../../dist/index.html'));
     })
 }
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use(express.static('../client/'))
+app.use(express.static('../client/dist/'))
 
 require('./routes/htmlRoutes.js')(app);
 
