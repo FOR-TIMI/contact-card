ffdac65303f4bbd4050fdcc4c1bf25f43372ffd0
FOR-TIMI <timiplayskeys@gmail.com>
2022-11-06 02:43:18 -0500

    added CREATE and READ functionality to indexDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -18,10 +18,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 /** IndexDB */
-import { initdb } from './database';
+
+
+import { initdb, getDb, postDb } from './database';
+
+//To get all data in the database
+
 
 window.addEventListener('load', function() {
   initdb();
+  getDb();
+  postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+  getDb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
